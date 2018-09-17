562028aee8c93e6d793d4326bbe3de27a5944c66
saicharanpogul <pogul1804@gmail.com>
2018-09-18 00:44:24 +0530

    mysql

--- users/connection.php
+++ users/connection.php
@@ -5,10 +5,10 @@
  * Date: 15-Sep-18
  * Time: 11:54 AM
  */
-$hostname="localhost";
-$username="root";
-$password="Pogul@1627";
-$database="creditmanagement";
+$hostname="us-cdbr-iron-east-01.cleardb.net";
+$username="b9590ee86b2d60";
+$password="4c2c99af";
+$database="heroku_63a3df67f940f3c";
 
 $connection = new mysqli($hostname,$username,$password,$database);
 if($connection->connect_error) {
