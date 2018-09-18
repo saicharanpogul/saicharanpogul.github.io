4f064ed596cf3adf6b40a454c5b5fd1a309c91d6
Saicharan Pogul <36988048+saicharanpogul@users.noreply.github.com>
2018-09-18 16:27:23 +0530

    username

--- users/user1/transfer1.php
+++ users/user1/transfer1.php
@@ -47,13 +47,13 @@ if (isset($_POST['submit'])) {
     if ($_POST['submit'] && is_numeric($_POST['Credits'])) {
 
 
-        $result = mysqli_query($connection, "UPDATE users SET Credits = Credits + " . $_POST['Credits'] . " WHERE UserId = " . $_POST['to']);
+        $result = mysqli_query($connection, "UPDATE users SET Credits = Credits + " . $_POST['Credits'] . " WHERE UserName = " . $_POST['to']);
         if ($result !== TRUE) {
             mysqli_rollback($connection);
         }
 
 
-        $result = mysqli_query($connection, "UPDATE users SET Credits = Credits - " . $_POST['Credits'] . " WHERE UserId = " . $_POST['from']);
+        $result = mysqli_query($connection, "UPDATE users SET Credits = Credits - " . $_POST['Credits'] . " WHERE UserName = " . $_POST['from']);
         if ($result !== TRUE) {
             mysqli_rollback($connection);
         }
@@ -81,7 +81,7 @@ mysqli_close($connection);
     <select name="from" class="container form-control">
         <?php
         foreach ($users as $u) {
-            echo "<option value=\"" . $u['UserId'] . "\">" . $u['UserId'] . "</option>";
+            echo "<option value=\"" . $u['UserName'] . "\">" . $u['UserName'] . "</option>";
         }
         ?>
     </select>
@@ -91,7 +91,7 @@ mysqli_close($connection);
     <select name="to" class="container form-control">
         <?php
         foreach ($users as $u) {
-            echo "<option value=\"" . $u['UserId'] . "\">" . $u['UserId'] . "</option>";
+            echo "<option value=\"" . $u['UserName'] . "\">" . $u['UserName'] . "</option>";
         }
         ?>
     </select><br>
